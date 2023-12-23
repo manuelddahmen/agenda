f65902003527a346aba10db7da07a6d258506910
Manuel Daniel Dahmen <manuel.dahmen@gmx.com>
2023-12-23 11:27:02 +0100

    RELEASE 1.0. #3

--- src/db.php
+++ src/db.php
@@ -24,49 +24,13 @@ require_once "MyDB.php";
 require_once "functions.php";
 require_once '../vendor/autoload.php';
 require_once "AgendaUser.php";
+require_once "initIdHospitalise.php";
 global $days, $halfHour;
 global $db;
 
 use JetBrains\PhpStorm\NoReturn;
 use PHPSQLParser\PHPSQLParser;
 
-
-
-
-if(!isset($id_hospitalise)) {
-    $i=0;
-
-    $id_hospitalise = array();
-    if(isset($_GET["id_hospitalise"]) && is_scalar($_GET["id_hospitalise"])) {
-        foreach ($_GET as $key => $value) {
-            $id_hospitalise[$i] = $_GET["id_hospitalise"];
-            $i++;
-        }
-    } else if(isset($_GET["id_hospitalise"]) && is_array($_GET["id_hospitalise"])) {
-        foreach ($_GET as $key => $value) {
-            if (array_search($value, $id_hospitalise)===false) {
-                $id_hospitalise[$i] = $_GET["id_hospitalise"];
-                $i++;
-            }
-        }
-    }
-    foreach ($_GET as $key => $value) {
-        if(str_starts_with("id_hospitalise_", $key)) {
-            if (array_search($value, $id_hospitalise)===false) {
-                $id_hospitalise[$i] = $value;
-                $i++;
-            }
-        }
-    }
-    foreach ($_GET as $key => $value) {
-        if (str_starts_with("id_hospitalises_", $key)) {
-            if (array_search($value, $id_hospitalise) === false) {
-                $id_hospitalise[$i] = $value;
-                $i++;
-            }
-        }
-    }
-}
 
 $db = new MyDB();
 
